714cca4d52a382ecb221c05c6c2bd387d2e54f34
高强 <13325764@qq.com>
2018-12-21 22:39:35 +0800

    初始化提交

---
+++ moved/moved.web/src/main/java/com/gao/cloud/ProviderUserApplication.java
@@ -0,0 +1,11 @@
+package com.gao.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderUserApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
